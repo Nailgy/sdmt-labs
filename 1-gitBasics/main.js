2470c304011b20186dd30ba9d70a960131e3772f
Nailgy <ilyha.nali@gmail.com>
2023-03-06 21:25:39 +0200

    Added interactive mode

--- 1-gitBasics/main.js
+++ 1-gitBasics/main.js
@@ -1,7 +1,8 @@
+const readlineSync = require("readline-sync");
+
 const showRoots = (arrOfRoots) => {
   const numOfRoots = arrOfRoots.length;
-  const verb = numOfRoots >= 2 ? "are" : "is";
-  let answer = `There ${verb} ${numOfRoots} roots\n`;
+  let answer = `There are ${numOfRoots} roots\n`;
   for (i = 0; i < numOfRoots; i++) {
     answer += `x${i + 1} = ${arrOfRoots[i]}\n`;
   }
@@ -15,13 +16,42 @@ const solveQuadEquation = (a, b, c) => {
   console.log(`Equation is: (${a})x^2 + (${b})x + (${c}) = 0`);
 
   if (D > 0) {
-    const x1 = ((-b - sqrtD) / (2 * a)).toFixed(1);
-    const x2 = ((-b + sqrtD) / (2 * a)).toFixed(1);
+    const x1 = ((-b - sqrtD) / (2 * a)).toFixed(3);
+    const x2 = ((-b + sqrtD) / (2 * a)).toFixed(3);
     roots = [x1, x2];
   }
   if (D === 0) {
-    const x = (-b / (2 * a)).toFixed(1);
+    const x = (-b / (2 * a)).toFixed(3);
     roots = [x];
   }
   return roots;
 };
+
+const consoleRead = (key) => {
+  let inputValue = readlineSync.question(key);
+
+  while (isNaN(inputValue)) {
+    console.log(
+      `Error. Expected a valid real number, got ${inputValue} instead`
+    );
+    inputValue = readlineSync.question(key);
+  }
+  if (inputValue === "") {
+    console.log("Error. Enter some value");
+    inputValue = consoleRead(key);
+  }
+  if (key === "a = " && inputValue === "0") {
+    console.log("Error. a must not be equal to 0");
+    inputValue = consoleRead(key);
+  }
+  return inputValue;
+};
+
+const runInteractiveMode = () => {
+  const keys = ["a = ", "b = ", "c = "];
+  const inputValues = keys.map((el) => consoleRead(el));
+  const roots = solveQuadEquation(...inputValues);
+  showRoots(roots);
+};
+
+runInteractiveMode();
